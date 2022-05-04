1bfc8b1fe5237fc2bf437a1c2912e5f68592528d
young <mryuanyoung@outlook.com>
2022-05-04 21:20:19 +0800

    fix: 修复鉴权功能
    Signed-off-by: young <mryuanyoung@outlook.com>

--- src/Controller/ProxyController.ts
+++ src/Controller/ProxyController.ts
@@ -1,48 +1,53 @@
 import { FileStat, ResponseDataDetailed } from "webdav";
-import { Autowired, PostMapping, RequestBody, RestController } from "../decorator/Request"
-import { CreateFile, GetFileReq } from "../dto/operator"
-import {Response} from '../dto/response';
-import WebDAV from "../WebDAV/client"
-
+import {
+  Autowired,
+  PostMapping,
+  RequestBody,
+  RestController,
+} from "../decorator/Request";
+import { CreateFile, GetFileReq } from "../dto/operator";
+import { Response } from "../dto/response";
+import WebDAV from "../WebDAV/client";
 
 @RestController
 class ProxyController {
+  @Autowired
+  WebDAV: WebDAV;
 
-    @Autowired
-    WebDAV: WebDAV
+  @PostMapping("/createFile")
+  async creatFile(@RequestBody req: CreateFile): Promise<Response<boolean>> {
+    const res = await this.WebDAV.createFile(req);
+    return {
+      success: res,
+      message: res ? "√" : "×",
+      res,
+    };
+  }
 
-    @PostMapping('/createFile')
-    async creatFile(@RequestBody req: CreateFile): Promise<Response<boolean>>{
-        const res = await this.WebDAV.createFile(req);
-        return {
-            success: res,
-            message: res ? '√' : '×',
-            res
-        }
-    }
+  @PostMapping("/files")
+  // todo 这里引用的类型前端框架里没有怎么办
+  async getFiles(
+    @RequestBody req: GetFileReq
+  ): Promise<Response<FileStat[] | ResponseDataDetailed<FileStat[]>>> {
+    const res = await this.WebDAV.getDirContent(req);
+    return {
+      success: !!res,
+      message: !!res + "",
+      res,
+    };
+  }
 
-    @PostMapping('/files')
-    // todo 这里引用的类型前端框架里没有怎么办
-    async getFiles(@RequestBody req: GetFileReq): Promise<Response<FileStat[] | ResponseDataDetailed<FileStat[]>>>{
-        const res = await this.WebDAV.getDirContent(req);
-        return {
-            success: !!res,
-            message: !!res+'',
-            res
-        }
-    }
+  @PostMapping("/fileContent")
+  async getFileContent(
+    @RequestBody req: GetFileReq
+  ): Promise<Response<string>> {
+    const res = await this.WebDAV.getFileContent(req);
+    return {
+      success: res !== "",
+      message: (res !== "") + "",
+      res: res as string,
+    };
+  }
+}
 
-    @PostMapping('/fileContent')
-    async getFileContent(@RequestBody req: GetFileReq):Promise<Response<string>>{
-        const res = await this.WebDAV.getFileContent(req);
-        return {
-            success: res !== '',
-            message: (res !== '') + '',
-            res: (res as string)
-        }
-    }
-
-    
-}   
-
-export default ProxyController
+export default ProxyController;
--- src/WebDAV/client.ts
+++ src/WebDAV/client.ts
@@ -1,55 +1,71 @@
-import { CreateFile, GetFileReq } from 'src/dto/operator';
-import {createClient, WebDAVClient} from 'webdav';
-import {DAVClient} from '../decorator/webdav';
+import { CreateFile, GetFileReq } from "src/dto/operator";
+import { AuthType, createClient, FileStat, WebDAVClient } from "webdav";
+import { DAVClient } from "../decorator/webdav";
 
 @DAVClient
 class WebDAV {
-    private client: WebDAVClient
+  private client: WebDAVClient;
 
-    constructor(){
+  constructor() {}
+
+  createClient(username: string, password: string, url?: string) {
+    this.client = createClient(url || "http://box.nju.edu.cn/seafdav", {
+      authType: AuthType.Password,
+      username,
+      password,
+    });
+  }
+
+  hasClient() {
+    return !!this.client;
+  }
+
+  async createFile(req: CreateFile) {
+    if (!this.hasClient()) {
+      this.createClient(req.username, req.password);
     }
 
-    createClient(username: string, password: string, url?:string){
-        this.client = createClient(
-            url || "https://box.nju.edu.cn/seafdav",
-            {
-                username,
-                password
-            }
-        );
+    const date = new Date();
+    try {
+      const res = await this.client.putFileContents(
+        `/BookkeepingData/${date.getFullYear()}-${
+          date.getMonth() + 1
+        }-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}.json`,
+        req.json,
+        { overwrite: true, contentLength: false }
+      );
+      return res;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  async getDirContent(req: GetFileReq) {
+    if (!this.hasClient()) {
+      this.createClient(req.username, req.password);
     }
 
-    hasClient(){
-        return !!this.client;
+    const res = await this.client.getDirectoryContents(
+      req.path || "/BookkeepingData/"
+    );
+    (res as Array<FileStat>).sort((a, b) => {
+      const da = new Date(a.lastmod);
+      const db = new Date(b.lastmod);
+      return db.getTime() - da.getTime();
+    });
+    return (res as Array<FileStat>).slice(0, 5);
+  }
+
+  async getFileContent(req: GetFileReq) {
+    if (!this.hasClient()) {
+      this.createClient(req.username, req.password);
     }
 
-    async createFile(req: CreateFile){
-        if(!this.hasClient()){
-            this.createClient(req.username, req.password);
-        }
-
-        const date = new Date();
-        const res = await this.client.putFileContents(`/BookkeepingData/${date.getUTCFullYear()}-${date.getUTCMonth()+1}-${date.getUTCDate()}.json`, req.json, {overwrite: true, contentLength: false});
-        return res;
-    }
-
-    async getDirContent(req: GetFileReq){
-        if(!this.hasClient()){
-            this.createClient(req.username, req.password);
-        }
-
-        const res = await this.client.getDirectoryContents(req.path || '/BookkeepingData/');
-        return res;
-    }
-
-    async getFileContent(req: GetFileReq){
-        if(!this.hasClient()){
-            this.createClient(req.username, req.password);
-        }
-
-        const res = await this.client.getFileContents(req.filename, {format: 'text'});
-        return res;
-    }
+    const res = await this.client.getFileContents(req.filename, {
+      format: "text",
+    });
+    return res;
+  }
 }
 
 export default WebDAV;
